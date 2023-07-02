7eb4269c1f4195fbe9c23b30f2046ff27f3e7ef8
Ali Badran <ali.badran53@yahoo.com>
2023-07-02 21:40:21 +0400

    Fixing HTTP client issue

--- app/src/main/java/com/example/cmp354project/LoggedIn.java
+++ app/src/main/java/com/example/cmp354project/LoggedIn.java
@@ -1,5 +1,7 @@
 package com.example.cmp354project;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,6 +13,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.gms.common.util.IOUtils;
+import com.google.firebase.appcheck.interop.BuildConfig;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpHeaders;
 
 import java.io.*;
 import java.net.*;
@@ -30,7 +34,7 @@ public class LoggedIn extends AppCompatActivity implements View.OnClickListener
 
     ArrayAdapter<CharSequence> adapter;
 
-    String tempString = "";
+    String jsonStr = "";
 
 
     //UPDATEE EVERY DAY
@@ -71,9 +75,25 @@ public class LoggedIn extends AppCompatActivity implements View.OnClickListener
 
                 try {
                     api_url[0] = new URL("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + api_key);
-                    HttpURLConnection huc = (HttpURLConnection) api_url[0].openConnection();
-                    InputStream inputStream = huc.getInputStream();
-                    String tempString = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+                    // HttpHeaders headers = new HttpHeaders();//headers.add("Accept","/");
+                    // HttpURLConnection huc = (HttpURLConnection) api_url[0].openConnection();
+                    //InputStream inputStream = huc.getInputStream();
+                    StringBuilder builder = new StringBuilder();
+                    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(api_url[0].openStream(), UTF_8))) {
+                        String str;
+                        while ((str = bufferedReader.readLine()) != null) {
+                            builder.append(str);
+                            builder.append(System.getProperty("line.separator"));                        }
+                    }
+                    String jsonStr = builder.toString();
+                    summonerLevel.setText(jsonStr);
+
+                   // for(int i=1;i<=8;i++){
+                        //tempString = tempString + (huc.getOutputStream().);
+                    //}
+
+
+
                 }
                 catch (Exception ex) {
                     ex.printStackTrace();
@@ -81,11 +101,12 @@ public class LoggedIn extends AppCompatActivity implements View.OnClickListener
             }
         }).start();
 
-        summonerLevel.setText(tempString);
+
 
 
 
 
     }
+
 
 }
