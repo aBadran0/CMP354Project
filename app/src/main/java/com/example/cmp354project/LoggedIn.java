6c0bab005a8c1d6e4ff293226932886bd240bc59
Ali Badran <ali.badran53@yahoo.com>
2023-07-02 14:54:43 +0400

    Login page annd initial testing of Riot API

---
+++ app/src/main/java/com/example/cmp354project/LoggedIn.java
@@ -0,0 +1,91 @@
+package com.example.cmp354project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.common.util.IOUtils;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+
+public class LoggedIn extends AppCompatActivity implements View.OnClickListener
+{
+
+    Button searchButton;
+    EditText et_accountName;
+    Spinner regionSelect;
+
+    TextView summonerLevel;
+
+    ArrayAdapter<CharSequence> adapter;
+
+    String tempString = "";
+
+
+    //UPDATEE EVERY DAY
+    String api_key = "RGAPI-b7298e94-8e67-4274-9d74-46206f11e3b2";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+
+        regionSelect = findViewById(R.id.regionSelect);
+        searchButton = findViewById(R.id.searchButton);
+        et_accountName = findViewById(R.id.et_accountName);
+        summonerLevel = findViewById(R.id.summonerLevel);
+
+        searchButton.setOnClickListener(this);
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.Regions, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        regionSelect.setAdapter(adapter);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String userName = et_accountName.getText().toString();
+        final URL[] api_url = {null};
+
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+
+                try {
+                    api_url[0] = new URL("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + api_key);
+                    HttpURLConnection huc = (HttpURLConnection) api_url[0].openConnection();
+                    InputStream inputStream = huc.getInputStream();
+                    String tempString = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining("\n"));
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+        summonerLevel.setText(tempString);
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/cmp354project/loginScreen.java
@@ -0,0 +1,116 @@
+package com.example.cmp354project;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class loginScreen extends AppCompatActivity implements View.OnClickListener
+{
+
+     FirebaseAuth mAuth;
+     EditText et_Email_Input;
+     EditText et_Password_Input;
+     Button btn_login;
+     Button btn_sign_up;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+        mAuth = FirebaseAuth.getInstance();
+        et_Email_Input= findViewById(R.id.et_Email_Input);
+        et_Password_Input= findViewById(R.id.et_Password_Input);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_sign_up = findViewById(R.id.btn_sign_up);
+
+        btn_login.setOnClickListener(this);
+        btn_sign_up.setOnClickListener(this);
+
+    }
+
+        @Override
+        public void onClick(View v)
+        {
+
+
+            if (et_Password_Input.getText().toString().isEmpty()) return;
+
+            if (v.getId()==R.id.btn_login) {
+                Log.d("Hello", "AAA");
+
+                //Create a new signIn method which takes in an email address and password,
+                // validates them, and then signs a user in with the signInWithEmailAndPassword method.
+                mAuth.signInWithEmailAndPassword(et_Email_Input.getText().toString(), et_Password_Input.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent i = new Intent(getApplicationContext(), LoggedIn.class);
+                                    i.putExtra("username", user.getEmail());
+                                    startActivity(i);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+            //----------------------------------------
+            //Create a new signIn method which takes in an email address and password,
+            // validates them, and then signs a user in with the signInWithEmailAndPassword method.
+            else if (v.getId()==R.id.btn_sign_up)
+            {
+                Log.d("Hi", "AAA");
+                mAuth.createUserWithEmailAndPassword(et_Email_Input.getText().toString(), et_Password_Input.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Toast.makeText(getApplicationContext(), "Create User With Email : success\nPlease login",
+                                            Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.d(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        }
+
+
+}
+
+
