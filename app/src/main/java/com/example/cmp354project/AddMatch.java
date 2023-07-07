42b7e62aa6673a407bb5a8660bfbe5159b7b916f
Ali Badran <ali.badran53@yahoo.com>
2023-07-07 21:45:45 +0400

    new xml and new java file

---
+++ app/src/main/java/com/example/cmp354project/AddMatch.java
@@ -0,0 +1,15 @@
+package com.example.cmp354project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddMatch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.adding_match_to_history);
+
+
+    }
+}
