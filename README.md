edccc79a33da6afb0a9ba9dabe6cc5d2ac96b2bb
rayyankhan01 <rayyanabadan@gmail.cmo>
2023-07-07 17:53:32 +0400

    not letting me do anything on the xml, gonna try commiting then trying

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CMP354Project
-This application utilises Firebase to allow the user to input their "Match History" manually, and search for other users who have also used the app to input their matches.
+This application utilises Firebase to allow the user to input their "Match History" manually, 
+and search for other users who have also used the app to input their matches.
 
--- app/src/main/java/com/example/cmp354project/LoggedIn.java
+++ app/src/main/java/com/example/cmp354project/LoggedIn.java
@@ -1,7 +1,10 @@
 package com.example.cmp354project;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -10,17 +13,35 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.android.gms.common.util.IOUtils;
+import com.google.firebase.appcheck.interop.BuildConfig;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpHeaders;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 
 public class LoggedIn extends AppCompatActivity implements View.OnClickListener
 {
 
-    Button setAccount;
+    Button searchButton;
     EditText et_accountName;
     Spinner regionSelect;
 
-    TextView tv_MainAccount;
+    Button setupButton;
+
+    TextView summonerLevel;
+
     ArrayAdapter<CharSequence> adapter;
 
+    String jsonStr = "";
+
+
+    //UPDATEE EVERY DAY -- not needed
+    String api_key = "";
 
 
     @Override
@@ -29,12 +50,14 @@ public class LoggedIn extends AppCompatActivity implements View.OnClickListener
         setContentView(R.layout.activity_logged_in);
 
         regionSelect = findViewById(R.id.regionSelect);
-        setAccount = findViewById(R.id.setAccount);
+        searchButton = findViewById(R.id.btn_searchAccount);
+        setupButton = findViewById(R.id.btn_setAccount);
+
         et_accountName = findViewById(R.id.et_accountName);
-        tv_MainAccount = findViewById(R.id.tv_MainAccount);
+    //    summonerLevel = findViewById(R.id.summonerLevel);
 
-        setAccount.setOnClickListener(this);
-
+        searchButton.setOnClickListener(this);
+        setupButton.setOnClickListener(this);
         adapter = ArrayAdapter.createFromResource(this, R.array.Regions, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -47,11 +70,60 @@ public class LoggedIn extends AppCompatActivity implements View.OnClickListener
     @Override
     public void onClick(View v) {
 
-        String currentUser = tv_MainAccount.getText().toString();
-        if (currentUser.equals("Currently No Account Set"))
+        if(v.getId() == R.id.btn_setAccount)
         {
-            tv_MainAccount.setText("Your account is currently " + et_accountName.getText().toString() + "In region " + regionSelect.getSelectedItem().toString());
+
+            Intent searchIntent = new Intent(this,AccountSetup.class);
+
+            startActivity(searchIntent);
         }
+
+
+
+
+
+
+        // do we even need any of this anymore ?
+
+/*        String userName = et_accountName.getText().toString();
+        final URL[] api_url = {null};
+
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+
+                try {
+                    api_url[0] = new URL("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName + "?api_key=" + api_key);
+                    // HttpHeaders headers = new HttpHeaders();//headers.add("Accept","/");
+                    // HttpURLConnection huc = (HttpURLConnection) api_url[0].openConnection();
+                    //InputStream inputStream = huc.getInputStream();
+                    StringBuilder builder = new StringBuilder();
+                    try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(api_url[0].openStream(), UTF_8))) {
+                        String str;
+                        while ((str = bufferedReader.readLine()) != null) {
+                            builder.append(str);
+                            builder.append(System.getProperty("line.separator"));                        }
+                    }
+                    String jsonStr = builder.toString();
+                    summonerLevel.setText(jsonStr);
+
+                   // for(int i=1;i<=8;i++){
+                        //tempString = tempString + (huc.getOutputStream().);
+                    //}
+
+
+
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();*/
+
+
+
+
 
 
     }
